1ba7f83d6c9e8f004d53517966f4ef0d9cbdd359
erica olivia <ecaoliviaaa@gmail.com>
2024-10-03 00:36:23 +0800

    fix:add offset and number params in axios third party api request

--- controllers/RecipeController.js
+++ controllers/RecipeController.js
@@ -1,7 +1,23 @@
+const axios = require('axios');
+
 class RecipeController{
     static async showRecipes(req,res,next){
         try{
-
+            // * TODO: nanti perlu nyalain ini
+            // const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch', {
+            //     params: {
+            //       addRecipeNutrition: true,
+            //       apiKey: process.env.SPOONACULAR_API_KEY,
+            //       offset: req.query.offset,  
+            //       number: req.query.number   
+            //     }
+            //   });
+        
+            //   const recipes = response.data;
+            //   res.status(200).json(recipes);
+            // * TODO: sementara langsung ambil dari json, ntar perlu tembak api asli
+            const recipes = require('../datas/complexSearch.json');
+            res.status(200).json(recipes);
         } catch (err){
             next(err);
         }
--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -1,5 +1,6 @@
 const recipeRouter = require("express").Router();
 const RecipeController = require("../controllers/RecipeController");
 
+recipeRouter.get("/", RecipeController.showRecipes);
 
 module.exports = recipeRouter;
